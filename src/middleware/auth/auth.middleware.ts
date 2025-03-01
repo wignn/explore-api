90b2fd674554c97390ac540cc0e3522149932440
wignn <tigfiragnafatur1933@gmail.com>
2025-03-01 16:15:10 +0700

    p

--- src/middleware/auth/auth.middleware.ts
+++ src/middleware/auth/auth.middleware.ts
@@ -6,11 +6,9 @@ export class ApiKeyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const apiKey = req.headers['x-api-key']; 
     const validApiKey = process.env.API_KEY; 
-
     if (!apiKey || apiKey !== validApiKey) {
       throw new UnauthorizedException('Invalid API Key');
     }
-
     next();
   }
 }
