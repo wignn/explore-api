b28d6b3c5796c6fbd7428cdeada4483004e18a19
wignn <tigfiragnafatur1933@gmail.com>
2025-03-01 15:35:54 +0700

    p

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { ApiKeyMiddleware } from './middleware/auth/auth.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
   app.enableCors();
+  app.use(new ApiKeyMiddleware().use);
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
